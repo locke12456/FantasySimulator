2ea7dcf11b3bfea0c3648a188aab4cadc452cb73
Locke Chen <locke12456@gmail.com>
2021-07-12 14:08:51 +0800

    add utils.

---
+++ utility/PostitonDection.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.utility
+{
+    class PostitonDection
+    {
+        public enum Position
+        {
+            None = 0,
+            TopLeft = 1,
+            TopRight = 2,
+            BottomLeft = 3,
+            BottomRight = 4,
+        };
+        private GameObject Role;
+        private float top_ref = 0.6f;
+        private float bottom_ref = -0.6f;
+        private bool onLeft = false;
+        private bool onRight = true;
+        private bool onTop = false;
+        private bool onBottom = false;
+        private Transform transform;
+        private Position position;
+        public Position GetPosition()
+        {
+            return position;
+        }
+        public PostitonDection(GameObject target)
+        {
+            transform = target.transform;
+            position = position_detection();
+            switch (position)
+            {
+                case Position.TopLeft:
+                case Position.BottomLeft:
+                    onLeft = true;
+                    onRight = false;
+                    break;
+                case Position.BottomRight:
+                case Position.TopRight:
+                    onLeft = false;
+                    onRight = true;
+                    break;
+            }
+        }
+
+
+        public Vector3 GetScaleHint(Quaternion rotation, float scale_base)
+        {
+            Vector3 scale = Vector3.zero;
+            position = position_detection();
+            switch (position)
+            {
+                case Position.TopLeft:
+                    if (!onLeft)
+                    {
+                        scale = (new Vector3(0, scale_base * -2f, 0));
+                        onLeft = true;
+                        onRight = false;
+                    }
+                    break;
+                case Position.BottomLeft:
+                    if (!onLeft)
+                    {
+                        scale = (new Vector3(0, scale_base * -2f, 0));
+                        onLeft = true;
+                        onRight = false;
+                    }
+                    break;
+                case Position.BottomRight:
+                    if (!onRight)
+                    {
+                        scale = (new Vector3(0, scale_base * 2f, 0));
+                        onLeft = false;
+                        onRight = true;
+                    }
+                    break;
+                case Position.TopRight:
+                    if (!onRight)
+                    {
+                        scale = (new Vector3(0, scale_base * 2f, 0));
+                        onLeft = false;
+                        onRight = true;
+                    }
+                    break;
+            }
+            return scale;
+        }
+        private Position position_detection()
+        {
+            if (transform.rotation.z < 0)
+            {
+                onTop = false;
+                onBottom = true;
+            }
+            else
+            {
+                onTop = true;
+                onBottom = false;
+            }
+            if (onBottom)
+            {
+                if (transform.rotation.z > bottom_ref)
+                {
+                    return Position.BottomRight;
+                }
+                if (transform.rotation.z < bottom_ref)
+                {
+                    return Position.BottomLeft;
+                }
+            }
+            if (onTop)
+            {
+                if (transform.rotation.z < top_ref)
+                {
+                    return Position.TopRight;
+                }
+                if (transform.rotation.z > top_ref)
+                {
+                    return Position.BottomLeft;
+                }
+            }
+            return Position.None;
+        }
+    }
+}
+
---
+++ utility/TilemapUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts.utility
+{
+    public class TilemapUtils
+    {
+        public Tilemap tilemap;
+        public TilemapUtils()
+        {
+
+        }
+        public Tile GetTileWithPosition(Vector3 target)
+        {
+            Vector3Int position = getPositionFromTilemap(target);
+
+            Tile tile = tilemap.GetTile<Tile>(position);
+
+            return tile;
+        }
+
+        public bool RemoveTileWithPositon(Vector3 target)
+        {
+            Vector3Int position = getPositionFromTilemap(target);
+
+            bool exist = tilemap.HasTile(position);
+            if(exist)
+            {
+                tilemap.SetTile(position, null);
+            }
+            return exist;
+        }
+        private Vector3Int getPositionFromTilemap(Vector3 target)
+        {
+            Vector3 worldPoint = getWorldPoint(target);
+
+            Vector3Int position = tilemap.WorldToCell(worldPoint);
+            return position;
+        }
+        private Vector3 getWorldPoint(Vector3 target)
+        {
+            // save the camera as public field if you using not the main camera
+            Ray ray = Camera.main.ScreenPointToRay(target);
+            // get the collision point of the ray with the z = 0 plane
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            return worldPoint;
+        }
+
+    }
+}
